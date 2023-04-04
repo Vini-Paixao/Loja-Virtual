66d190fc3ae044f2c22f3905030f6179b8a06884
Marcus Vinicius <marcuspaixao0@gmail.com>
2023-04-04 16:35:12 -0300

    Update conexão.php

--- conexão.php
+++ conexão.php
@@ -1,7 +1,7 @@
 <?php
-    $Servidor = 'https://sh-pro58.hostgator.com.br:2083/cpsess8159630461/3rdparty/phpMyAdmin/db_structure.php?server=1&db=hotecc21_loja-virtual';
-    $Usuario = 'hotecc21';
-    $Senha = '92m0WruV6e';
-    $BD = 'hotecc21_loja-virtual';
+    $Servidor = 'localhost';
+    $Usuario = 'emperium';
+    $Senha = '123456';
+    $BD = 'dt_LojaEmperium';
     $cn = new PDO("mysql:host=$Servidor; dbname=$BD", $Usuario, $Senha);
 ?>
